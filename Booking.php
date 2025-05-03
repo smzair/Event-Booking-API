8e51e96c8dfed698e5a63221f54b35ae91aeda6a
smzair <113309515+smzair@users.noreply.github.com>
2025-05-03 18:28:18 +0530

    Create Booking.php

---
+++ Booking.php
@@ -0,0 +1,24 @@
+
+  namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'event_id', 'attendee_id'
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function attendee()
+    {
+        return $this->belongsTo(Attendee::class);
+    }
+}
